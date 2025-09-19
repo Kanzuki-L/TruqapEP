9def8c88666300220fe85396b98b5ec67de049ce
Kanzuki <kennylim8979@gmail.com>
2025-09-19 12:03:20 +0800

    updated new video

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "Evoweb-TruqapEP-0.0.1";
 const contentToCache = [
     "Build/6a3995434de98db861218953d140c345.loader.js",
     "Build/795ac81d0fcdaf8da11e2745e1c930d8.framework.js.unityweb",
-    "Build/06c0df0e5922259628f831a8d4175892.data.unityweb",
+    "Build/6bb72e76f627c766c6cd1db04528c8fa.data.unityweb",
     "Build/2c1fb4c9a2aec6305538f3922ec925f6.wasm.unityweb",
     "TemplateData/style.css"
 
