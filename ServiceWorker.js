29a9a83f09251e3839d82257d8067dffa5b959a7
Kanzuki <kennylim8979@gmail.com>
2025-09-21 19:03:14 +0800

    updated button i to I

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "Evoweb-TruqapEP-0.0.1";
 const contentToCache = [
     "Build/d9d9830623ab0dae121296b0a44cb2f6.loader.js",
     "Build/5dfbd1306a74518362440e5983f7b1e9.framework.js.unityweb",
-    "Build/1d5601c5d94e43cc2a49db8a5b56cf07.data.unityweb",
+    "Build/a9911935a4b70e60b84a46e31abd1aed.data.unityweb",
     "Build/d931adebfc6f29d24cc8b0377760252b.wasm.unityweb",
     "TemplateData/style.css"
 
