a42e19d466540b31ad078d0fb8374c870c2db0e1
Kanzuki <kennylim8979@gmail.com>
2025-09-21 16:24:01 +0800

    fixed sprite missing and update on the roadsign texture

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,8 +2,8 @@ const cacheName = "Evoweb-TruqapEP-0.0.1";
 const contentToCache = [
     "Build/d9d9830623ab0dae121296b0a44cb2f6.loader.js",
     "Build/5dfbd1306a74518362440e5983f7b1e9.framework.js.unityweb",
-    "Build/ee466d7b295a9ff68ac0c83f79f1b474.data.unityweb",
-    "Build/b2490a5ce91c90a1b3fe805bc51f0764.wasm.unityweb",
+    "Build/1d5601c5d94e43cc2a49db8a5b56cf07.data.unityweb",
+    "Build/d931adebfc6f29d24cc8b0377760252b.wasm.unityweb",
     "TemplateData/style.css"
 
 ];
