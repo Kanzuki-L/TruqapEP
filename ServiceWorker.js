d76dcec15c113c32bbd6fba1a55a5db73321c7ba
Kanzuki <kennylim8979@gmail.com>
2025-09-23 17:28:42 +0800

    removed unity logo

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -2,7 +2,7 @@ const cacheName = "Evoweb-TruqapEP-0.0.1";
 const contentToCache = [
     "Build/09d5daab403c7db0168e42ff42cd5f28.loader.js",
     "Build/5dfbd1306a74518362440e5983f7b1e9.framework.js.unityweb",
-    "Build/8eac540d473e383a1591a6ff15d83bac.data.unityweb",
+    "Build/8e16d17a98aff6e6f869d99a00c8465d.data.unityweb",
     "Build/8dc04937bb29fda4fc162a09edb03533.wasm.unityweb",
     "TemplateData/style.css"
 
